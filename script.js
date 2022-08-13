11d07754b5b438f7721fd3a6b2dd1293215d4ace
Sol Rojas <solrojascaero14@gmail.com>
2022-08-12 23:40:22 -0300

    Segunda entre del proyecto final

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const cuentaUser = document.getElementById(`cuentaUser`)
 const productos = document.getElementById(`productos`)
+const boton = document.getElementById(`boton`)
+const clientes = document.getElementById("clientes")
+const verUsuarios = document.getElementById("verUsuarios")
+const consultas = document.getElementById("consultas")
+const eliminarConsulta = document.getElementById("eliminarConsulta")
 
 class Producto {
     constructor (nombre, precio) {
         this.nombre = nombre;
         this.precio = precio;
+    }
+}
+class Cliente {
+    constructor (email, nombre, descripcion) {
+        this.email = email;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
     }
 }
 
@@ -15,10 +26,13 @@ const instrumento4 = new Producto ("Trompeta 🎷", 60000)
 const instrumento5 = new Producto ("Saxofon 🎺", 90000)
 
 const instrumentos = [instrumento1, instrumento2, instrumento3, instrumento4, instrumento5]
+let usuarios = []
 
 productos.addEventListener(`click`, () => {
+
+    boton.innerHTML = ""
             instrumentos.forEach(instrumento => {
-                botone.innerHTML += `
+                boton.innerHTML += `
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                        <h5 class="card-title">${instrumento.nombre}</h5>
@@ -30,48 +44,48 @@ productos.addEventListener(`click`, () => {
             })
 })
 
+if (localStorage.getItem("usuarios")) {
 
-cuentaUser.addEventListener(`click`, () => {
-    botones.innerHTML =
-    `<div class="tituloContacto">
-            <h2>CONTACTO</h2>
-            <p class="contacto">¡No dudes en consultarnos ante cualquier duda!</p>
-            <p class="contacto">SOUNDSARC@GMAIL.COM</p>
-            <a class="contacto" href="https://api.whatsapp.com/send/?phone=5491144948424&text&app_absent=0">TEL: 011 4494 8424</a>
-        </div>
-        <form class="row g-3">
-            <div class="col-md-6">
-                <input type="email" class="form-control" id="inputEmail4" placeholder="Name@gmail.com">
-            </div>
-            <div class="col-md-6">
-                <input class="form-control" id="Nombre" placeholder="Nombre">
-            </div>
-            <div class="col-md-6">
-                <input type="text" class="form-control" id="observaciones" placeholder="Tengo una duda con..">
-            </div>
-            <div class="col-md-4">
-                <label for="localidades" class="form-label">Localidad</label>
-                <select id="inputState" class="form-select">
-                    <option selected>Seleccione..</option>
-                    <option>CABA</option>
-                    <option>Ezeiza</option>
-                    <option>Lanús</option>
-                    <option>La Matanza</option>
-                    <option>Lomas de Zamora</option>
-                    <option>Merlo</option>
-                    <option>Moreno</option>
-                    <option>Quilmes</option>
-                </select>
-            </div>
-            <div class="col-12">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox">
-                    <label class="form-check-label" for="gridCheck">Recibir más información</label>
-                </div>
-            </div>
-            <div class="col-12">
-                <button type="submit" class="btn btn-dark">Enviar</button>
-            </div>
-        </form>
-        `
+    usuarios = JSON.parse(localStorage.getItem("usuarios"))
+}else{
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+}
+
+clientes.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const datForm = new FormData (evento.target)
+    const consulta = new Cliente (datForm.get("email"), datForm.get("nombre"), datForm.get("descripcion"))
+
+    usuarios.push(consulta)
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+
+    clientes.reset()
+})
+
+verUsuarios.addEventListener(`click`, () => {
+
+    const usuariosStorage = JSON.parse(localStorage.getItem("usuarios"))
+
+    consultas.innerHTML = ""
+
+    usuariosStorage.forEach((consulta, indice) => {
+        consultas.innerHTML += `
+        <div class="clicks" id ="consulta${indice}" style="width: 18rem;">
+           <div class="card-body">
+                <h5 class="card-title">${consulta.nombre}</h5>
+                <p class="card-text"> ${consulta.descripcion}</p>
+                <button class="btn btn-danger">Eliminar</button>
+           </div>
+       </div>`
+    })
+
+    usuariosStorage.forEach((consulta, indice) => {
+        document.getElementById(`consulta${indice}`).children[0].children[2].addEventListener(`click`, () => {
+            document.getElementById(`consulta${indice}`).remove()
+            usuarios.splice(indice,1)
+            localStorage.setItem(`usuarios`, JSON.stringify(usuarios))
+        })
+    })
 })
