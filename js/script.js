26817f3b532eb25da3e1d1c058bab98215e1715b
Sol Rojas <solrojascaero14@gmail.com>
2022-08-24 22:29:42 -0300

    Octavo desafio

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
-const productos = document.getElementById(`productos`)
 const boton = document.getElementById(`boton`)
 
-class Producto {
-    constructor (nombre, precio) {
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-}
+fetch('./json/productos.json')
+.then(response => response.json())
 
-const instrumento1 = new Producto ("Piano 🎹", 28000)
-const instrumento2 = new Producto ("Guitarra electrica 🎸", 39000)
-const instrumento3 = new Producto ("Violin 🎻", 18000)
-const instrumento4 = new Producto ("Trompeta 🎷", 60000)
-const instrumento5 = new Producto ("Saxofon 🎺", 90000)
-
-const instrumentos = [instrumento1, instrumento2, instrumento3, instrumento4, instrumento5]
-
-productos.addEventListener(`click`, () => {
-
-    boton.innerHTML = ""
-            instrumentos.forEach(instrumento => {
-                boton.innerHTML += `
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                       <h5 class="card-title">${instrumento.nombre}</h5>
-                       <p class="card-text">Precio: $${instrumento.precio}</p
-                   </div>
-               </div>               `
-            })
+.then(productos => {
+    productos.forEach((producto, indice) => {
+      boton.innerHTML += `
+      <div class="card" id= "producto${indice}">
+        <img src="${producto.img}" class="card-img-top" alt="producto">
+        <div class="card-body">
+           <h5 class="card-title">${producto.nombre}</h5>
+           <p class="card-text">Marca: ${producto.marca}</p>
+           <p class="card-text">Precio: $${producto.precio}</p>
+           <p class="card-text">Stock: ${producto.stock}</p>
+           <button class="btn comprar btn-dark">Comprar</button>
+        </div>
+      </div>
+      `
+    })
 })
